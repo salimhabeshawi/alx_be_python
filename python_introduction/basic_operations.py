395f30d3146c2457a54957c8f3f95206bb5a8178
salimhabeshawi <salimhabeshawi@gmail.com>
2025-09-10 23:04:24 +0300

    create separate print statements

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,10 @@
+number1 = 10
+number2 = 5
 
+tot = number1 + number2
+diff = number1 - number2
+prod = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {tot}")
+print(f"Subtraction of {number1} and {number2} is {diff}")
+print(f"Multiplication of {number1} and {number2} is {prod}")
