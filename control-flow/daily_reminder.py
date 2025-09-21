375fd78c0c5ee0f6a2ce10fd3e0556ea499184da
salimhabeshawi <salimhabeshawi@gmail.com>
2025-09-21 16:46:38 +0300

    feat: implement daily reminder task input with priority and time-bound checks

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,26 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(
+                f"'{task}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
+            print(f"'{task}' is a {priority} priority task. Consider completeing when you have time.")
+        else:
+            print("Wrong input!")
+    case "medium":
+        if time_bound == "yes":
+            print(f"'{task}' is a {priority} priority task. Consider completeing it today!")
+        elif time_bound == "no":
+            print(f"'{task}' is a {priority} priority task. Consider completeing when you have time.")
+        else:
+            print("Wrong input!")
+    case "low":
+        if time_bound == "yes":
+            print(f"'{task}' is a {priority} priority task. Consider completeing it today!")
+        elif time_bound == "no":
+            print(f"'{task}' is a {priority} priority task. Consider completeing when you have time.")
+        else:
+            print("Wrong input!")
