444e1ba008f3e115cb4237195b2b91dc6102f9bc
salimhabeshawi <salimhabeshawi@gmail.com>
2025-09-21 16:56:18 +0300

    fix: enhance reminder messages for task priority and time-bound checks

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,22 +5,22 @@ match priority:
     case "high":
         if time_bound == "yes":
             print(
-                f"'{task}' is a {priority} priority task that requires immediate attention today!")
+                f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         elif time_bound == "no":
-            print(f"'{task}' is a {priority} priority task. Consider completeing when you have time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completeing when you have time.")
         else:
             print("Wrong input!")
     case "medium":
         if time_bound == "yes":
-            print(f"'{task}' is a {priority} priority task. Consider completeing it today!")
+            print(f"Reminder: '{task}' is a {priority} priority task. Consider completeing it today!")
         elif time_bound == "no":
-            print(f"'{task}' is a {priority} priority task. Consider completeing when you have time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completeing when you have time.")
         else:
             print("Wrong input!")
     case "low":
         if time_bound == "yes":
-            print(f"'{task}' is a {priority} priority task. Consider completeing it today!")
+            print(f"Reminder: '{task}' is a {priority} priority task. Consider completeing it today!")
         elif time_bound == "no":
-            print(f"'{task}' is a {priority} priority task. Consider completeing when you have time.")
+            print(f"Note: '{task}' is a {priority} priority task. Consider completeing when you have time.")
         else:
             print("Wrong input!")
