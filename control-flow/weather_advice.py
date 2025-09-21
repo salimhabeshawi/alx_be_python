b2445038dc0d426b0283bfb49d77e0e945470b4f
salimhabeshawi <salimhabeshawi@gmail.com>
2025-09-21 11:26:26 +0300

    feat: add weather advice program based on user input

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+usr_input = input("What's the weather like today? (sunny/rainy/cold): ")
+if usr_input == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif usr_input == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif usr_input == "sunny":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
