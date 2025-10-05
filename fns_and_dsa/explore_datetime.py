00137c7620250f4a7182f92a8f0b70ffeb8c946a
salimhabeshawi <salimhabeshawi@gmail.com>
2025-10-05 14:02:21 +0300

    feat: add functionality to display current and future dates

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    return current_date
+
+def calculate_future_date(number_of_days):
+    future_date = datetime.now() + timedelta(days=number_of_days)
+    return future_date
+
+print(f"Current date and time: {display_current_datetime()}")
+number_of_days = int(input("Enter the number of days to add to the current date: "))
+print(f"Future date: {calculate_future_date(number_of_days)}")
