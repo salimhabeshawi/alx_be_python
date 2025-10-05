8bec195a5c237b7a978620ac23e8dbaab287b75d
salimhabeshawi <salimhabeshawi@gmail.com>
2025-10-05 14:05:11 +0300

    fix: format date output in display_current_datetime and calculate_future_date functions

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,12 +3,15 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    return current_date
+    return current_date.strftime("%Y-%m-%d %H:%M:%S")
+
 
 def calculate_future_date(number_of_days):
     future_date = datetime.now() + timedelta(days=number_of_days)
-    return future_date
+    return future_date.strftime("%Y-%m-%d %H:%M:%S")
+
 
 print(f"Current date and time: {display_current_datetime()}")
-number_of_days = int(input("Enter the number of days to add to the current date: "))
+number_of_days = int(
+    input("Enter the number of days to add to the current date: "))
 print(f"Future date: {calculate_future_date(number_of_days)}")
