6b2859cad7bb6da7b2ff6191633f94e9f15c73ab
salimhabeshawi <salimhabeshawi@gmail.com>
2025-10-23 09:34:52 +0300

    feat: add string representation method to Book class

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,6 +2,8 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"Book('{self.title}', '{self.author}')"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
